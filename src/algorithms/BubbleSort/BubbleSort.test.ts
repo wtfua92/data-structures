bcdc96dca7008ed584c3081628d107c2803322b3
wtfua92 <andriytinok@gmail.com>
2020-06-10 08:22:00 +0300

    Commented bubble sort v2 while investigating

--- src/algorithms/BubbleSort/BubbleSort.test.ts
+++ src/algorithms/BubbleSort/BubbleSort.test.ts
@@ -22,15 +22,15 @@ describe("BubbleSortV1", () => {
   });
 });
 
-describe("BubbleSortV2", () => {
-  it("should sort an array", function () {
-    const arr = [3, 4, 2, 1, 5];
-    const result = [1, 2, 3, 4, 5];
-    expect(BubbleSortV2(arr)).toEqual(result);
-  });
-
-  it("should return the same array if it's already sorted", () => {
-    const result = [1, 2, 3, 4, 5];
-    expect(BubbleSortV2(result)).toEqual(result);
-  });
-});
+// describe("BubbleSortV2", () => {
+//   it("should sort an array", function () {
+//     const arr = [3, 4, 2, 1, 5];
+//     const result = [1, 2, 3, 4, 5];
+//     expect(BubbleSortV2(arr)).toEqual(result);
+//   });
+//
+//   it("should return the same array if it's already sorted", () => {
+//     const result = [1, 2, 3, 4, 5];
+//     expect(BubbleSortV2(result)).toEqual(result);
+//   });
+// });
--- src/algorithms/BubbleSort/BubbleSort.ts
+++ src/algorithms/BubbleSort/BubbleSort.ts
@@ -23,17 +23,17 @@ export function BubbleSortV1(arr: number[]): number[] {
 
   return newArr;
 }
-
-export function BubbleSortV2(arr: number[]): number[] {
-  let newArr = [...arr];
-
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1; j++) {
-      if (newArr[j] > newArr[i]) {
-        newArr = swap(newArr, i, j);
-      }
-    }
-  }
-
-  return newArr;
-}
+//
+// export function BubbleSortV2(arr: number[]): number[] {
+//   let newArr = [...arr];
+//
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length - 1; j++) {
+//       if (newArr[j] > newArr[i]) {
+//         newArr = swap(newArr, i, j);
+//       }
+//     }
+//   }
+//
+//   return newArr;
+// }
