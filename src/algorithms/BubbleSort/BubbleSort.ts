8ed5bd3bea6417f50e033976d843c3fb33922c15
wtfua92 <andriytinok@gmail.com>
2020-06-10 10:52:40 +0300

    Added comments

--- src/algorithms/BubbleSort/BubbleSort.ts
+++ src/algorithms/BubbleSort/BubbleSort.ts
@@ -1,5 +1,19 @@
 import { swap } from "../tools/tools";
 
+/*
+    1. Set swapCounter to be not 0;
+    2. While swapCounter is not 0:
+        2.1. Set swapCounter to be 0;
+        2.2. For each element in array
+            2.2.1 Compare each element to the element next to it;
+                - If current element > next element
+                    Swap these elements;
+                    Increase swapCounter by 1;
+
+    On the last iteration, when the array is already sorted,
+    the swapCounter will become 0 again, but it will not change,
+    so the loop will break and we can be sure that the array is sorted.
+ */
 export function bubbleSortV1(arr: number[]): number[] {
   let newArr = [...arr];
   let swapCounter = -1;
--- src/algorithms/SelectionSort/SelectionSort.ts
+++ src/algorithms/SelectionSort/SelectionSort.ts
@@ -1,5 +1,17 @@
 import { swap } from "../tools/tools";
 
+/*
+    Outer loop tracks the unsorted part of an array;
+
+        Inner loop goes through the unsorted part of an array
+        and compares current smallest value with each element in the array:
+            if current smallest value is > current element of the array
+                assign current element's index to be the smallest value;
+
+    Once the smallest element index is found
+    swap it with the first element of the unsorted part of the array;
+*/
+
 export function selectionSort(arr: number[]): number[] {
   let newArr = [...arr];
 
